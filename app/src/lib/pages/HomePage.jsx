ea822508641538ca5df1f256f6d56ce230342e6f
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 21:50:40 +0200

    React <> Add - quizzes mapping on homePage

--- app/src/lib/pages/HomePage.jsx
+++ app/src/lib/pages/HomePage.jsx
@@ -1,9 +1,22 @@
+import { useSelector } from "react-redux";
 import Header from "../../ui/components/Header";
 import "./../../ui/styles/app.css";
+
 const HomePage = () => {
+  const { quizzes } = useSelector((state) => state.quizzes);
   return (
     <>
       <Header />
+      <div className="flex justify-center gap">
+        {quizzes.map((q, index) => (
+          <QuizCard
+            key={index}
+            id={q.id}
+            title={q.title}
+            description={q.description}
+          />
+        ))}
+      </div>
     </>
   );
 };
