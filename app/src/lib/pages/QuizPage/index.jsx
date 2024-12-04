b6901e9144f0c2d974fd0f79fa5faba6a4e61753
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 22:44:34 +0200

    React <> Add - quiz properties in Quizpage

--- app/src/lib/pages/QuizPage/index.jsx
+++ app/src/lib/pages/QuizPage/index.jsx
@@ -1,13 +1,19 @@
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const QuizPage = () => {
-  const quizzes = useSelector((state) => state.quizzes);
+  const { quizzes } = useSelector((state) => state.quizzes);
   const { id } = useParams();
-  const quiz = quizzes.find((q) => q.id === id);
-
+  const quiz = quizzes.find((q) => q.id === parseInt(id));
+  console.log("id is ", id, " of type ", typeof id);
+  console.log("Quizzes is ", quizzes);
+  console.log("Quiz is ", quiz);
   return (
     <>
-      <p>{quiz}</p>
+      <div className="flex column gap pd-20">
+        <h2>{quiz.title}</h2>
+        <h3>{quiz.questions.length} Questions</h3>
+      </div>
     </>
   );
 };
