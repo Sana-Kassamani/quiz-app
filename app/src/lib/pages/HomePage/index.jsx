a0e3c04a521979bf4d35a6442ac7466923f38abd
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 22:21:06 +0200

    React <> Move - HomePage jsx to index.jsx in HomePage folder

--- app/src/lib/pages/HomePage/index.jsx
+++ app/src/lib/pages/HomePage/index.jsx
@@ -1,9 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Header from "../../ui/components/Header";
 import "./../../ui/styles/app.css";
+import { useEffect } from "react";
+import { loadQuizzes } from "../../local_data_source/redux/quizzes/slice";
+import { getQuizzes } from "../../remote_data_source/quizApi";
 
 const HomePage = () => {
   const { quizzes } = useSelector((state) => state.quizzes);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const quizzes = getQuizzes();
+    dispatch(loadQuizzes(quizzes));
+  }, []);
   return (
     <>
       <Header />
