6210c9051607adaeabb30fde2787c8fedef5ccd9
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 21:50:21 +0200

    React <> Add - quiz slice to redux store

--- app/src/lib/local_data_source/redux/store.js
+++ app/src/lib/local_data_source/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/slice.js";
+import quizzesReducer from "./quizzes/slice.js";
 
 const store = configureStore({
   reducer: {
     users: userReducer,
+    quizzes: quizzesReducer,
   },
 });
 
