be9c26876d9a4c52e35ee687957d839183adcbd7
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 21:49:13 +0200

    React <> Create - quiz card component to display quizzes on homePage

---
+++ app/src/ui/components/QuizCard/index.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+const QuizCard = ({ id, title, description }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/quiz/${id}`);
+  };
+  return (
+    <div className="flex column gap">
+      <h2>{title}</h2>
+      <h4>{description}</h4>
+      <button onClick={handleClick}>Sit for quiz</button>
+    </div>
+  );
+};
