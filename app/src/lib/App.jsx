54c5d7500b4eed9d1b2c7a38133242bb24165683
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 20:56:31 +0200

    React <> Debug - file paths in app and index

--- app/src/lib/App.jsx
+++ app/src/lib/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./../ui/styles/App.css";
 import { Provider } from "react-redux";
+import store from "./local_data_source/redux/store";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
