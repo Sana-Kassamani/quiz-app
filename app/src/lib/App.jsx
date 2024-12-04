6e98f9e8366326ecd0c9311b55b891d30b723ad6
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 22:17:58 +0200

    React <> Add - quizPage route in app.jsx

--- app/src/lib/App.jsx
+++ app/src/lib/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/quiz/:id" element={<QuizPage />} />
           </Routes>
         </BrowserRouter>
       </Provider>
