1707448a8e5f383ef45b545f4778b3d2b667c4f2
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 23:58:47 +0200

    React <> Add - updateAnswer reducer to quiz slice

--- app/src/lib/local_data_source/redux/quizzes/slice.js
+++ app/src/lib/local_data_source/redux/quizzes/slice.js
@@ -13,9 +13,30 @@ const quizSlice = createSlice({
         quizzes: payload,
       };
     },
+    updateAnswer: (current, { payload }) => {
+      return {
+        quizzes: current.quizzes.map((quiz) => {
+          if (quiz.id == payload.quizId) {
+            return {
+              ...quiz,
+              questions: quiz.questions.map((question, index) => {
+                if (index == payload.questionIndex) {
+                  return {
+                    ...question,
+                    userAnswer: payload.answer,
+                  };
+                }
+                return question;
+              }),
+            };
+          }
+          return quiz;
+        }),
+      };
+    },
   },
 });
 
 export default quizSlice.reducer;
 
-export const { loadQuizzes } = quizSlice.actions;
+export const { loadQuizzes, updateAnswer } = quizSlice.actions;
