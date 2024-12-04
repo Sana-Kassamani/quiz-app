810cdfb505b2dc12d71fe045f4420fb7865edb74
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-04 23:59:29 +0200

    React <> Edit - quiz page to use hook useGetQuestion and add a submit button

--- app/src/lib/pages/QuizPage/index.jsx
+++ app/src/lib/pages/QuizPage/index.jsx
@@ -1,19 +1,24 @@
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useGetQuestion } from "../../../hooks/useGetQuestion";
+import QuestionCard from "../../../ui/components/QuestionCard";
+import "../../../ui/styles/app.css";
 
 const QuizPage = () => {
-  const { quizzes } = useSelector((state) => state.quizzes);
-  const { id } = useParams();
-  const quiz = quizzes.find((q) => q.id === parseInt(id));
-  console.log("id is ", id, " of type ", typeof id);
-  console.log("Quizzes is ", quizzes);
-  console.log("Quiz is ", quiz);
+  const { title, questions } = useGetQuestion();
+  const handleSubmit = () => {
+    console.log(questions);
+  };
   return (
     <>
       <div className="flex column gap pd-20">
-        <h2>{quiz.title}</h2>
-        <h3>{quiz.questions.length} Questions</h3>
+        <h2>{title}</h2>
+        <h3>{questions.length} Questions</h3>
       </div>
+      <div>
+        {questions.map((q, index) => (
+          <QuestionCard index={index} key={index} />
+        ))}
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
     </>
   );
 };
