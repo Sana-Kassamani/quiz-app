b8f61ca6ce22e63c6c36dd75efa3a42958e2af1e
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-05 05:01:51 +0200

    React <> Update - quiz slice and remove handling an answer

--- app/src/lib/local_data_source/redux/quizzes/slice.js
+++ app/src/lib/local_data_source/redux/quizzes/slice.js
@@ -13,30 +13,31 @@ const quizSlice = createSlice({
         quizzes: payload,
       };
     },
-    updateAnswer: (current, { payload }) => {
-      return {
-        quizzes: current.quizzes.map((quiz) => {
-          if (quiz.id == payload.quizId) {
-            return {
-              ...quiz,
-              questions: quiz.questions.map((question, index) => {
-                if (index == payload.questionIndex) {
-                  return {
-                    ...question,
-                    userAnswer: payload.answer,
-                  };
-                }
-                return question;
-              }),
-            };
-          }
-          return quiz;
-        }),
-      };
-    },
   },
 });
 
 export default quizSlice.reducer;
 
-export const { loadQuizzes, updateAnswer } = quizSlice.actions;
+export const { loadQuizzes } = quizSlice.actions;
+
+// updateAnswer: (current, { payload }) => {
+//     return {
+//       quizzes: current.quizzes.map((quiz) => {
+//         if (quiz.id == payload.quizId) {
+//           return {
+//             ...quiz,
+//             questions: quiz.questions.map((question, index) => {
+//               if (index == payload.questionIndex) {
+//                 return {
+//                   ...question,
+//                   userAnswer: payload.answer,
+//                 };
+//               }
+//               return question;
+//             }),
+//           };
+//         }
+//         return quiz;
+//       }),
+//     };
+//   },
