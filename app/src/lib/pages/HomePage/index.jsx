6ae6237721db576c08f3cb42783a19f5e7239f72
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-05 05:03:51 +0200

    React <> Move - loadQuizzes from homePage to QuizzesRoute

--- app/src/lib/pages/HomePage/index.jsx
+++ app/src/lib/pages/HomePage/index.jsx
@@ -1,19 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import Header from "../../../ui/components/Header";
 import "../../../ui/styles/app.css";
-import { useEffect } from "react";
-import { loadQuizzes } from "../../local_data_source/redux/quizzes/slice";
-import { getQuizzes } from "../../remote_data_source/quizApi";
 import QuizCard from "../../../ui/components/QuizCard";
 
 const HomePage = () => {
   const { quizzes } = useSelector((state) => state.quizzes);
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    const quizzes = getQuizzes();
-    dispatch(loadQuizzes(quizzes));
-  }, []);
   console.log(quizzes);
   return (
     <>
