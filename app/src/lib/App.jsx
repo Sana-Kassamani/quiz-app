8a90ffc09c8b611cce970d5e75475ea132103903
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-05 05:00:36 +0200

    React <> Add - route for result page

--- app/src/lib/App.jsx
+++ app/src/lib/App.jsx
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import store from "./local_data_source/redux/store";
 import HomePage from "./pages/HomePage";
 import QuizPage from "./pages/QuizPage";
+import QuizzesRoute from "../ui/components/QuizzesRoute";
+import ResultPage from "./pages/ResultPage";
 
 function App() {
   return (
@@ -10,8 +12,11 @@ function App() {
       <Provider store={store}>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/quiz/:id" element={<QuizPage />} />
+            <Route path="/" element={<QuizzesRoute />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/quiz/:id" element={<QuizPage />} />
+              <Route path="/result" element={<ResultPage />} />
+            </Route>
           </Routes>
         </BrowserRouter>
       </Provider>
