550af40e5749bcce42d2dfefb25e53485d7e8696
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-05 05:04:33 +0200

    React <> Debug - useGetQuestion hook and add try catch

--- app/src/hooks/useGetQuestion.js
+++ app/src/hooks/useGetQuestion.js
@@ -4,6 +4,15 @@ import { useParams } from "react-router-dom";
 export const useGetQuestion = () => {
   const { id } = useParams();
   const { quizzes } = useSelector((state) => state.quizzes);
-  const { title, questions } = quizzes.find((q) => q.id === parseInt(id));
-  return { title, questions };
+
+  try {
+    if (quizzes) {
+      const { title, questions } = quizzes.find((q) => q.id === parseInt(id));
+      return { title, questions };
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 };
