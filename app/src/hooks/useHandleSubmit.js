a4f640716da3ff342fed6670ff578e36c1256552
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-05 05:05:27 +0200

    React <> Create - useHandleSubmit hook to manage submitting a quiz

---
+++ app/src/hooks/useHandleSubmit.js
@@ -0,0 +1,43 @@
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  finishQuiz,
+  updateScore,
+} from "../lib/local_data_source/redux/user/slice";
+import { useDispatch } from "react-redux";
+
+export const useHandleSubmit = ({ setError, questions, answers }) => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  function handleSubmit() {
+    var score = 0;
+    var fullScore = 0;
+    var count = 0;
+    try {
+      answers.forEach((a, index) => {
+        if (!a) {
+          throw Error("All questions are required");
+        }
+        if (a === questions[index].correctAnswer) {
+          score += questions[index].score;
+          count += 1;
+        }
+        fullScore += questions[index].score;
+      });
+      dispatch(finishQuiz({ id, score }));
+      dispatch(updateScore());
+      navigate("/result", {
+        state: {
+          score,
+          fullScore,
+          count,
+          fullCount: questions.length,
+        },
+      });
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+  return { handleSubmit };
+};
