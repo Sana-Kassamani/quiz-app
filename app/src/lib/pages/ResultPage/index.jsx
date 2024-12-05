5830faa740d42abcd74d63226d07a222cdf46c35
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-05 05:00:58 +0200

    React <> Create - a result page

---
+++ app/src/lib/pages/ResultPage/index.jsx
@@ -0,0 +1,23 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+const ResultPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { score, fullScore, count, fullCount } = location.state || {};
+  const handleClick = () => {
+    navigate("/");
+  };
+  return (
+    <div className="flex column full-height justify-center align-center gap">
+      <h2>
+        {count} out of {fullCount} Questions are correct.
+      </h2>
+      <h2>
+        Score: {score} / {fullScore}
+      </h2>
+      <button onClick={handleClick}>Back to Home</button>
+    </div>
+  );
+};
+
+export default ResultPage;
