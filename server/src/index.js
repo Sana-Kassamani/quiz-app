99dd19cce8faf5c8444ff9093fb2ee5c635f0286
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-07 00:43:40 +0200

    Node <> Remove - app.use for routes from index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
 import authRouter from "./routes/auth.routes.js";
-import { connectToDb } from "./db/connection.js";
-import { authmiddleware } from "./middlewares/auth.middleware.js";
 import userRouter from "./routes/user.routes.js";
 import quizRouter from "./routes/quiz.route.js";
+import { connectToDb } from "./db/connection.js";
+import { initApp, registerRoutes } from "./utils/init.js";
 
 const app = express();
-app.use(express.json());
-dotenv.config();
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: "Hello World",
-//   });
-// });
-app.use("/auth", authRouter);
-app.use("/user", authmiddleware, userRouter);
-app.use("/quiz", authmiddleware, quizRouter);
-
+initApp(app);
+registerRoutes(app, authRouter, userRouter, quizRouter);
 app.listen(8080, async () => {
   console.log("Server connected at port ", 8080);
   await connectToDb();
