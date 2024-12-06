73aa5c90a570eea352d5bf17ccb4b4c041b00779
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-06 06:39:08 +0200

    Node <> Create - question schema

---
+++ server/src/db/schemas/question.schema.js
@@ -0,0 +1,26 @@
+import { Schema, model, Types } from "mongoose";
+
+const questionSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["mcq", "input"],
+  },
+  options: [
+    {
+      type: String,
+    },
+  ],
+  correctAnswer: {
+    type: String,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: true,
+  },
+});
+export default questionSchema;
