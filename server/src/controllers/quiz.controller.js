0dd03e8b4d6bba736a211801c0a3327edcf4c286
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-06 23:09:58 +0200

    Node <> Add - create quiz function

--- server/src/controllers/quiz.controller.js
+++ server/src/controllers/quiz.controller.js
@@ -1,7 +1,27 @@
 import Quiz from "../db/models/quiz.model.js";
+import { validateFields } from "../utils/validation.js";
+
 export const getQuiz = async (req, res) => {
   const quizzes = await Quiz.find();
   return res.status(200).json({
     quizzes,
   });
+};
+
+export const createQuiz = async (req, res) => {
+  const { title } = req.body.title;
+
+  try {
+    validateFields(title);
+    const quiz = await Quiz.create({ title, questions: [] });
+
+    return res.status(201).json({
+      message: "quiz created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error adding user",
+    });
+  }
 };
