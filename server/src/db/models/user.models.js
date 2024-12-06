47655e50a96cdd82b1ebcd8417a8b16ee953cf80
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-06 05:03:04 +0200

    Node <> Add - user model to access in auth controllers

---
+++ server/src/db/models/user.models.js
@@ -0,0 +1,30 @@
+import { Schema, model, Types } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: true,
+    select: false,
+  },
+  score: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  //   quizzes: [{
+  //     ref:
+  //   }],
+});
+
+const User = model("User", userSchema);
+export default User;
