72138934be384f5a3d761129b4fcc46269e84e7c
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-06 05:18:54 +0200

    Node <> Add - register function in auth controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,27 +1,38 @@
 import bcrypt from "bcrypt";
 import User from "../db/models/user.models.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { username, name, password } = req.body;
 
   if (!username || !name || !password) {
     res.status(400).json({
+      req: req.body,
       message: "All fields are required",
+    });
+  }
+  const pastUser = await User.findOne({ username });
+  if (pastUser) {
+    return res.status(500).send({
+      message: "Username already registered",
     });
   }
   try {
     const salt = await bcrypt.genSalt(10);
-    const hashed = await bcrypt.hash(password, 10);
+    const hashed = await bcrypt.hash(password, salt);
 
     const user = await User.create({
       username,
-      hashed,
+      password: hashed,
       name,
     });
 
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
     res.status(201).json({
-      message: "All fields are required",
+      message: "User added succesfully",
       user: user,
+      token: token,
     });
   } catch (error) {
     console.log(error);
