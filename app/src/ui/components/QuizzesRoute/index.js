5638162f555d353f177ae8486983bab9dd6f8135
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-07 04:00:19 +0200

    React <> Add - get quizzes api to hydrate quizzes slice

--- app/src/ui/components/QuizzesRoute/index.js
+++ app/src/ui/components/QuizzesRoute/index.js
@@ -2,14 +2,30 @@ import React, { useEffect, useState } from "react";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loadQuizzes } from "../../../lib/local_data_source/redux/quizzes/slice.js";
-import { getQuizzes } from "../../../lib/remote_data_source/quizApi.js";
+import { getQuizzes } from "./services.js";
+import { request } from "../../../utils/request";
 
 const QuizzesRoute = () => {
+  const [quizzes, setQuizzes] = useState([]);
   const dispatch = useDispatch();
+  const getQuizzes = async () => {
+    try {
+      const response = await request({
+        route: "/quiz",
+      });
+      console.log(response);
+
+      if (response.status === 200) {
+        setQuizzes(response.data.quizzes);
+        dispatch(loadQuizzes(response.data.quizzes));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    const quizzes = getQuizzes();
-    console.log("Quizzes route is called");
-    dispatch(loadQuizzes(quizzes));
+    getQuizzes();
+    console.log("Quizzes route is called ", quizzes);
   }, []);
 
   return <Outlet />;
