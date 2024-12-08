5d79061300ac10c19484e7ef60b9530f0a28108d
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-08 10:54:38 -0300

    React <> Add - extraReducer to load quizzes in slice

--- app/src/lib/local_data_source/redux/quizzes/slice.js
+++ app/src/lib/local_data_source/redux/quizzes/slice.js
@@ -1,24 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchQuizzes } from "../../../remote_data_source/fetchQuizzes";
 
 const quizInitialState = {
   quizzes: [],
+  isLoading: false,
+  error: null,
 };
 
 const quizSlice = createSlice({
   name: "quizzes",
   initialState: quizInitialState,
-  reducers: {
-    loadQuizzes: (current, { payload }) => {
-      return {
-        quizzes: payload,
-      };
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchQuizzes.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchQuizzes.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.quizzes = action.payload.quizzes;
+    });
+    builder.addCase(fetchQuizzes.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    });
   },
 });
 
 export default quizSlice.reducer;
 
-export const { loadQuizzes } = quizSlice.actions;
+// export const { loadQuizzes } = quizSlice.actions;
 
 // updateAnswer: (current, { payload }) => {
 //     return {
