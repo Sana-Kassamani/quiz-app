4d525f34ea6e6339b655dfe76e62a9148abab3c8
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-08 10:58:28 -0300

    React <> Add - dispatch actions in quizzesRoute

--- app/src/ui/components/QuizzesRoute/index.js
+++ app/src/ui/components/QuizzesRoute/index.js
@@ -1,48 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { loadQuizzes } from "../../../lib/local_data_source/redux/quizzes/slice.js";
-import { request } from "../../../utils/request";
-import { loadUser } from "../../../lib/local_data_source/redux/user/slice.js";
+
+import { fetchQuizzes } from "../../../lib/remote_data_source/fetchQuizzes.js";
+import { fetchUser } from "../../../lib/remote_data_source/fetchUser.js";
 
 const QuizzesRoute = () => {
-  const [quizzes, setQuizzes] = useState([]);
   const dispatch = useDispatch();
-  const getQuizzes = async () => {
-    try {
-      const response = await request({
-        route: "/quiz",
-      });
-      console.log(response);
 
-      if (response.status === 200) {
-        setQuizzes(response.data.quizzes);
-        dispatch(loadQuizzes(response.data.quizzes));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const loadUserApi = async () => {
-    try {
-      const response = await request({
-        route: "/user",
-      });
-      console.log(response);
-
-      if (response.status === 200) {
-        const user = response.data.user;
-        dispatch(loadUser({ name: user.name, score: user.score }));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
   useEffect(() => {
-    loadUserApi();
-    getQuizzes();
-    console.log("Quizzes route is called ", quizzes);
-  }, []);
+    dispatch(fetchUser());
+    dispatch(fetchQuizzes());
+    console.log("Quizzes route is called ");
+  }, [dispatch]);
 
   return <Outlet />;
 };
