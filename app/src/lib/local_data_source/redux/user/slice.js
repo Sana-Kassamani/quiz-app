0d6c97e620fe84c8aede9a4b5c6e22e9ae925ab0
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-08 10:56:46 -0300

    React <> Add - loading user to slice through extraReducer

--- app/src/lib/local_data_source/redux/user/slice.js
+++ app/src/lib/local_data_source/redux/user/slice.js
@@ -1,22 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUser } from "../../../remote_data_source/fetchUser";
 
 const userInitialState = {
   name: "",
   score: 0,
   quizzes: [],
+  isLoading: false,
+  error: null,
 };
 
 const userSlice = createSlice({
   name: "users",
   initialState: userInitialState,
   reducers: {
-    loadUser: (current, { payload }) => {
-      return {
-        ...current,
-        name: payload.name,
-        score: payload.score,
-      };
-    },
     finishQuiz: (current, { payload }) => {
       const found = current.quizzes.find((q) => q.id === payload.id);
       if (found) {
@@ -39,8 +35,22 @@ const userSlice = createSlice({
       };
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.name = action.payload.name;
+      state.score = action.payload.score;
+    });
+    builder.addCase(fetchUser.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    });
+  },
 });
 
 export default userSlice.reducer;
 
-export const { loadUser, updateScore, finishQuiz } = userSlice.actions;
+export const { updateScore, finishQuiz } = userSlice.actions;
