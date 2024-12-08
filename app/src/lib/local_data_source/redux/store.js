d899c1139021086a442be834584764e381d8837b
Sana <Lenovo@DESKTOP-I4OODFD>
2024-12-08 13:14:30 -0300

    React <> Add - redux logger

--- app/src/lib/local_data_source/redux/store.js
+++ app/src/lib/local_data_source/redux/store.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/slice.js";
 import quizzesReducer from "./quizzes/slice.js";
+import { createLogger } from "redux-logger";
 
+const logger = createLogger();
 const store = configureStore({
   reducer: {
     users: userReducer,
     quizzes: quizzesReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
